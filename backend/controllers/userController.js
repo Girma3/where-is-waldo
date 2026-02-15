2a10be54f4a17049802b6dc07ca128cb2bae2041
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-02-15 12:06:41 +0300

    feat: log out func and fetching user by id added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,8 @@ import {
   getUsers,
   deleteUserById,
   deleteUserByEmail,
+  getUserById,
+  getUserByEmail,
 } from "../db/userQueries.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -21,6 +23,32 @@ async function addUser(req, res) {
     return res.status(500).json({ error: "Failed to create user" });
   }
 }
+async function logInUser(req, res) {
+  const { email, name } = req.body;
+
+  if (!email || !name) {
+    return res.status(400).json({ error: "Email and name are required" });
+  }
+  try {
+    const user = await getUserByEmail(email);
+
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.status(200).json({ message: "Logged in" });
+    });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to log in user" });
+  }
+}
+async function logOutUser(req, res) {
+  try {
+    req.logout(() => {
+      res.status(200).json({ message: "Logged out" });
+    });
+  } catch (err) {
+    return res.status(500).json({ error: "Failed to log Out " });
+  }
+}
 
 async function fetchUsers(req, res) {
   try {
@@ -30,7 +58,18 @@ async function fetchUsers(req, res) {
     return res.status(500).json({ error: "Failed to fetch users" });
   }
 }
-
+async function fetchUserById(req, res) {
+  const { id } = req.params;
+  try {
+    const user = await getUserById(Number(id));
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to fetch user by id" });
+  }
+}
 async function removeUserById(req, res) {
   const { id } = req.params;
   if (!id) {
@@ -49,6 +88,7 @@ async function removeUserById(req, res) {
 
 async function removeUserByEmail(req, res) {
   const { email } = req.params;
+  console.log(email);
   try {
     const userToDelete = await deleteUserByEmail(email);
     if (!userToDelete) {
@@ -60,4 +100,12 @@ async function removeUserByEmail(req, res) {
   }
 }
 
-export { addUser, fetchUsers, removeUserById, removeUserByEmail };
+export {
+  addUser,
+  logInUser,
+  logOutUser,
+  fetchUsers,
+  fetchUserById,
+  removeUserById,
+  removeUserByEmail,
+};
