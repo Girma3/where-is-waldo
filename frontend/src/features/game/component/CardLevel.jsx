4e64c91ffa76c5cfa090ab7de3a490335180b6f9
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 12:09:50 +0300

    fix: card used as link and icon for characters added

--- frontend/src/features/game/component/CardLevel.jsx
+++ frontend/src/features/game/component/CardLevel.jsx
@@ -1,51 +1,56 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+const hardLevelStyle = `text-red-500  bg-red-100 rounded-sm p-2`;
+const easyLevelStyle = `text-green-500  bg-green-100 rounded-sm p-2`;
+const mediumLevelStyle = `text-yellow-500  bg-yellow-100  rounded-sm p-2`;
 
-const LEVEL_DATA = [
-  {
-    levelNumber: 1,
-    difficulty: "Easy",
-    characterNames: ["Waldo", "Odlaw", "Whitebeard"],
-    characterImages: ["/waldo-icon.png", "/wizard-icon.png", "/odlaw-icon.png"],
-  },
-];
-
-function CardLevel() {
-  const { levelNumber, difficulty, characterImages, characterNames } =
-    LEVEL_DATA[0];
+function CardLevel({ levelData: { level, difficulty, image, characters } }) {
+  let levelDifficultyStyle = "";
+  if (difficulty === "hard") {
+    levelDifficultyStyle = hardLevelStyle;
+  } else if (difficulty === "easy") {
+    levelDifficultyStyle = easyLevelStyle;
+  } else if (difficulty === "medium") {
+    levelDifficultyStyle = mediumLevelStyle;
+  }
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      whileHover={{ scale: 1.05, rotateX: 4 }}
-      transition={{ type: "spring", stiffness: 200, damping: 15 }}
-      className="flex flex-col items-center justify-center gap-2 w-48 bg-white cursor-pointer "
-    >
-      <img
-        src="./easy-level.png"
-        alt="waldo"
-        className="w-full h-32 object-cover rounded-sm"
-      />
+    <Link to={`/game/${level}`}>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        whileHover={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 60, damping: 15 }}
+        className="flex flex-col items-center justify-center gap-2 w-48 bg-white cursor-pointer"
+      >
+        <img
+          src={`${image}`}
+          alt={`Level ${level} image`}
+          className="w-full h-32 object-cover rounded-sm"
+        />
 
-      <p className="text-center font-bold text-white">
-        Level {levelNumber}{" "}
-        <span className="text-orange-400 font-semibold">{difficulty}</span>
-      </p>
+        <p className="font-semibold">
+          Level {level} -{" "}
+          <span className={levelDifficultyStyle}>{difficulty}</span>
+        </p>
 
-      <p className="text-sm text-white">Find this characters</p>
-      <ul className="flex flex-wrap gap-2 justify-center text-xs text-black">
-        {characterImages.map((imageUrl, i) => (
-          //
-          <li key={i}>
-            <img
-              src={imageUrl}
-              alt="character"
-              className="w-8 h-8 object-contain rounded-full shadow-sm"
-            />
-          </li>
-        ))}
-      </ul>
-    </motion.div>
+        <p className="text-sm text-gray-600">
+          Find these {characters.length > 1 ? "characters" : "character"}
+        </p>
+
+        <ul className="flex flex-wrap gap-2 justify-center text-xs text-black">
+          {characters.map(({ name, icon }) => (
+            <li key={name}>
+              <img
+                src={icon}
+                alt={name}
+                className="w-8 h-8 object-contain rounded-full shadow-sm"
+              />
+            </li>
+          ))}
+        </ul>
+      </motion.div>
+    </Link>
   );
 }
 
