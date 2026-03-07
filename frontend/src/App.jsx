eade686b1715d7653230e7722e6090f82f9f8901
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 12:09:02 +0300

    fix : react router used

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+
+const HomePage = () => <Home />;
+const GamePage = () => <Game />;
+
 function App() {
   return (
-    <>
-      <h1>where is waldo</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/game/:level" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
