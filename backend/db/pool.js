23237fbef231567d1d53a88a7b90e45a7d159fdf
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 12:15:51 +0300

    fix: prisma instance created only once to avoid error

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,37 +1,26 @@
 import dotenv from "dotenv";
-dotenv.config();
-import { PrismaPg } from "@prisma/adapter-pg";
+dotenv.config({ path: "./db/prisma/.env" });
+
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-if (!connectionString) {
-  throw new Error("Missing DATABASE_URL environment variable");
-}
-
-const adapter = new PrismaPg({ connectionString });
 const globalForPrisma = globalThis;
 
-const prismaGlobal =
-  globalForPrisma.prismaGlobal ||
-  new PrismaClient({
-    adapter,
-  });
+let prismaGlobal = globalForPrisma.prismaGlobal;
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prismaGlobal = prismaGlobal;
-}
+if (!prismaGlobal) {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
+    throw new Error("Missing DATABASE_URL");
+  }
 
-async function connectToDatabase() {
-  try {
-    await prismaGlobal.$connect();
-    console.log("Connected to the database successfully.");
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    throw error;
+  const adapter = new PrismaPg({ connectionString });
+
+  prismaGlobal = new PrismaClient({ adapter });
+
+  if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prismaGlobal = prismaGlobal;
   }
 }
-
-//connectToDatabase();
 
 export default prismaGlobal;
