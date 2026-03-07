54440e3e31f3de1dc8323c67ebf975a6c8b63dc2
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 12:16:26 +0300

    fix: cors added

--- backend/app.js
+++ backend/app.js
@@ -1,61 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./db/prisma/.env" });
+
 import express from "express";
 import expressSession from "express-session";
-import prismaGlobal from "./db/pool.js";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import passport from "./authentication/passportConfig.js";
+import prismaGlobal from "./db/pool.js";
+import cors from "cors";
 
 import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/usersRoute.js";
+import gameRouter from "./routes/gameRoute.js";
+import appApiInfo from "./controllers/appRouteInfo.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use((req, res, next) => {
-//   // Set the Access-Control-Allow-Origin header
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(
   expressSession({
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+      secure: false,
+      sameSite: "lax",
     },
     secret: "a santa at nasa",
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     store: new PrismaSessionStore(prismaGlobal, {
       checkPeriod: 2 * 60 * 1000, //ms
       dbRecordIdIsSessionId: true,
-      dbRecordIdFunction: undefined,
+      // dbRecordIdIsUserId: true,
     }),
   }),
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.get("/", (req, res) => {
-  return res.status(200).json("Welcome to the Where's Waldo API!");
-});
-// app.post("/login", passport.authenticate("local"), (req, res) => {
-//   res.json({ message: "Logged in", user: req.user });
-// });
-
-// app.post("/logout", (req, res) => {
-//   req.logout(() => {
-//     res.json({ message: "Logged out" });
-//   });
-// });
 
 app.use("/api", authRouter);
-app.use("/api", userRouter);
 
-// app.listen(port, () => {
-//   console.log(`Server is running, link http://localhost:${port}`);
-// });
+app.use("/api", userRouter);
+app.use("/api", gameRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running, link http://localhost:${port}`);
+});
 
 export default app;
