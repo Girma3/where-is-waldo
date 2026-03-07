0b6bd9902d6f033ea3b4619e797d031564965223
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 12:14:58 +0300

    fix: table accept data as prop to render leaderboard

--- frontend/src/features/leaderboard/component/Table.jsx
+++ frontend/src/features/leaderboard/component/Table.jsx
@@ -1,24 +1,38 @@
 import useFetchLeaderboard from "../hooks/useLeaderBoard";
-
-function Table() {
-  const tableData = useFetchLeaderboard();
+const tableStyle = `w-full  border border-gray-200 rounded-md shadow-sm`;
+const headerStyle = `whitespace-nowrap px-4 py-2 text-left text-sm font-semibold text-gray-700`;
+const rowStyle = `border-t hover:bg-gray-50 transition-colors`;
+const cellStyle = `p-3 text-sm text-gray-800 break-words`;
+function Table({ level }) {
+  const { data: tableData, error, isLoading } = useFetchLeaderboard(level);
+  if (!tableData?.length) {
+    return (
+      <p className="text-center text-gray-500 mt-4">
+        No leaderboard data available for this level.
+      </p>
+    );
+  }
 
   return (
     <div>
-      <table>
-        <thead>
+      <table className={tableStyle}>
+        <thead className="bg-gray-100">
           <tr>
-            <th>Rank</th>
-            <th>Name</th>
-            <th>time</th>
+            <th className={headerStyle}>Rank</th>
+            <th className={headerStyle}>Name</th>
+            <th className={headerStyle}>Time Taken</th>
+            <th className={headerStyle}>Achieved At</th>
           </tr>
         </thead>
         <tbody>
           {tableData.map((item, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{item.name}</td>
-              <td>{item.score}</td>
+            <tr key={index} className={rowStyle}>
+              <td className={cellStyle}>{index + 1}</td>
+              <td className={cellStyle}>{item.userName}</td>
+              <td className={cellStyle}>{item.timeTaken}s</td>
+              <td className={cellStyle}>
+                {new Date(item.achievedAt).toLocaleString()}
+              </td>
             </tr>
           ))}
         </tbody>
