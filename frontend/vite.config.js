ca5be3ec00dff17101b90948adbedc305491253d
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2026-03-07 14:52:31 +0300

    tailwindcss and vitest config added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    globals: true,
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
+});
